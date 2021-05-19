60dad6c28cfc930f50e94f1d9178d09391cc58b0
motovotcazsvs <78682549+motovotcazsvs@users.noreply.github.com>
2021-05-19 08:58:31 +0300

    jghf

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@
 #include <QDateTime>
 #include <QFileDialog>
 
-void writeFile(QString &, int);
+void writeFile(QString, int);
 void writeFile(QFile &, QString &);
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -16,32 +16,37 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QTextStream out(stdout);
     QString file_settings_path = "settingsfile.txt";
     QFile settings_file(file_settings_path);
+
     file_data_path = QDir::currentPath();
+    save_settings_comport = "com7";
     save_settings_speed = "9600";
+
     if(settings_file.exists(file_settings_path)){
         qDebug() << "Файл существует";
         QTextStream cin(&settings_file);
         QStringList file_settings_strings;
+
         if(settings_file.open(QIODevice::ReadOnly)){
             for(int i = 0; i < 3; i++) file_settings_strings << cin.readLine();
             settings_file.close();
         }
+
         file_data_path = file_settings_strings[0];
         save_settings_comport = file_settings_strings[1];
         save_settings_speed = file_settings_strings[2];
     }
     else {
         qDebug() << "Файл не существует!";
-        settings_file.open(QIODevice::WriteOnly | QIODevice::Text);
+        settings_file.open(QIODevice::WriteOnly); //| QIODevice::Text);
         QTextStream cout(&settings_file);
-        cout << file_data_path << endl;
-        cout << "com7" << endl;
-        cout << save_settings_speed << endl;
+        cout << file_data_path << "\n";
+        cout << save_settings_comport << "\n";
+        cout << save_settings_speed << "\n";
         settings_file.close();
     }
+
     qDebug() << file_data_path << endl << save_settings_comport << endl << save_settings_speed << endl;
     QDateTime dat;
     dat = QDateTime::currentDateTime();
@@ -84,24 +89,27 @@ void MainWindow::on_pushButton_clicked()
     qDebug() << open_folder;
 }
 
-void writeFile(QString &str, int num_str)
+void writeFile(QString str, int num_str)
 {
     QFile file("settingsfile.txt");
     QStringList strList;
     /*Считываем исходный файл в контейнер*/
-    if ((file.exists())&&(file.open(QIODevice::ReadOnly))){
+    if(file.open(QIODevice::ReadOnly)){
         while(!file.atEnd()){
             strList << file.readLine();
         }
         file.close();
     }
     /*Добавляем строку и сохраняем содержимое контейнера в тот же файл*/
-    if((file.exists())&&(file.open(QIODevice::WriteOnly))){
+    if(file.open(QIODevice::WriteOnly | QIODevice::Text)){
         strList[num_str] = str;
         QTextStream stream(&file);
         foreach(QString s, strList){
-            stream << s << endl;
             qDebug() << s;
+        }
+        foreach(QString s, strList){
+            stream << s;
+
         }
 
         file.close();
